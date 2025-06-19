9b3735d2b0f503e920040bebb35b0a297a817d92
chae1s <chae1zd.b@gmail.com>
2025-06-19 19:43:44 +0900

    feat: initial full-stack application setup

---
+++ pageon_backend/src/main/java/com/pageon/backend/PageonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pageon.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PageonBackendApplication.class, args);
+	}
+
+}
